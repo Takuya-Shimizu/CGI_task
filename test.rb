56f23b9a58579ea0da99dee73ec614f4b5368494
takuya shimizu <p.t.901.ts@gmail.com>
2022-11-24 15:22:16 +0900

    CGI再提出

--- test.rb
+++ test.rb
@@ -9,6 +9,6 @@ server = WEBrick::HTTPServer.new({
   Signal.trap(signal){ server.shutdown }
 }
 
-server.mount('/task', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.start
